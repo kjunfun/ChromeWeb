6506cee97640683b79c35e92814520e9c419b99a
JHLee <term273729@gmail.com>
2023-01-30 22:46:17 +0900

    Feat: JS EventListener공부 및 구현

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
-const title = document.getElementById("title");
-document.ge
+const testVar = document.querySelector(".hello h1:nth-child(2)");
 
-title.innerText = "Got u!"
-console.log(title.innerHTML)
+function handleTitleClick(){
+    console.log("testVar was clicked");
+}
+function handleMousEnter(){
+    testVar.innerText = "Mouse is here!";
+}
+function handleMouseLeave(){
+    testVar.innerText = "Mouse is gone!";
+}
+function handleWindowResize(){
+    document.body.style.backgroundColor = "tomato";
+}
+
+testVar.addEventListener("click", handleTitleClick); 
+testVar.onmouseenter = handleMousEnter; 
+testVar.addEventListener("mouseleave", handleMouseLeave); 
+window.addEventListener("resize", handleWindowResize);
